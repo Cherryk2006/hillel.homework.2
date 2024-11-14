885e9d099e0f04243c0484e03326d5b5569e0f1c
Cherryk2006 <anastasya.cherednyk@gmail.com>
2024-11-13 19:02:49 -0600

    initial commit refresh

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
-from flask import Flask, request
+import sqlite3
+
+from flask import Flask, request, render_template, redirect
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+class DB_local(object):
+    def __init__(self, file_name):
+        self.con = sqlite3.connect(file_name)
+        self.con.row_factory = dict_factory
+        self.cur = self.con.cursor()
+
+    def __enter__(self):
+        return self.cur
+
+    def __exit__(self, type, value, traceback):
+        self.con.commit()
+        self.con.close()
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
-        return 'GET'
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        with DB_local(request.files['db3.db']) as db_project:
+            db_project.execute('''SELECT * FROM user WHERE login = ? AND password = ?'''
+                               , (username, password))
+            user = db_project.fetchone()
+            if user:
+                return "Login Successful"
+            else:
+                return "Incorect Username or Password", 401
+
     if request.method == 'POST':
         return 'POST'
 
@@ -26,37 +60,71 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
-        return 'GET'
+        return render_template('register.html')
     if request.method == 'POST':
-        return 'POST'
+        with DB_local('db3.db') as db_cur:
+            form_data = request.form
+            db_cur.execute('''INSERT INTO users
+             (login, password, full_name, contacts, ipn, photo, passport) 
+             VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                           (
+                               form_data['login'], form_data['password'], form_data['full_name'],
+                               form_data['contacts'], form_data['ipn'], form_data['photo'], form_data['passport']
+                                       )
+                           )
+        return redirect('/login')
+
 
 @app.route('/items', methods=['GET', 'POST'])
 def items():
     if request.method == 'GET':
-        return 'GET'
+        with DB_local('db3.db') as db_cur:
+            db_cur.execute("SELECT * FROM item")
+            items = db_cur.fetchall()
+        return render_template(items.html)
     if request.method == 'POST':
-        return 'POST'
+        with DB_local('db3.db') as db_cur:
+            db_cur.execute('''INSERT INTO items (photo, name, description, price_hour, price_day, price_week, price_month)
+            VALUES (:photo, :name, :description, :price_hour, :price_day, :price_week, :price_month)''', request.form)
+        return redirect('/items')
 
 @app.route('/items/<int:item_id>', methods=['GET', 'DELETE'])
 def item_detail(item_id):
     if request.method == 'GET':
+        with DB_local('db3.db') as db_cur:
+            db_cur.execute("SELECT * FROM item WHERE id = ?", (item_id,))
+            item = db_cur.fetchone()
+            return render_template(item.html)
         return f'GET {item_id}'
     if request.method == 'DELETE':
-        return f'DELETE {item_id}'
+        with DB_local('db3.db') as db_cur:
+            db_cur.execute("DELETE FROM item WHERE id = ?", (item_id,))
+        return redirect('/items')
 
 @app.route('/leasers', methods=['GET'])
 def leasers():
-    return 'GET'
+    if request.method == 'GET':
+        with DB_local('db3.db') as db_cur:
+            db_cur.execute("SELECT * FROM leasers")
+            leasers = db_cur.fetchall()
+        return render_template("leasers.html")
 
 @app.route('/leasers/<int:leaser_id>', methods=['GET'])
 def leaser_detail(leaser_id):
+    with DB_local('db3.db') as db_cur:
+        db_cur.execute("SELECT * FROM leaser WHERE id = ?", (leaser_id,))
+        leaser = db_cur.fetchone()
     if request.method == 'GET':
-        return f'GET {leaser_id}'
+        return render_template("leasers.html")
 
 @app.route('/contracts', methods=['GET', 'POST'])
 def contracts():
     if request.method == 'GET':
-        return 'GET'
+        with DB_local('db3.db') as db_cur:
+            db_cur.execute("SELECT * FROM contracts")
+            contracts = db_cur.fetchall()
+        return render_template('contracts.html')
+
     if request.method == 'POST':
         return 'POST'
 
@@ -93,7 +161,7 @@ def compare():
 @app.route('/profile', methods=['GET', 'PUT/PATCH', 'DELETE'])
 def profile():
     if request.method == 'GET':
-        return 'GET'
+        return render_template("user.html")
     if request.method == 'PUT':
         return 'PUT'
     if request.method == 'PATCH':
